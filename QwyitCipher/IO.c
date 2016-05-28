b574bf5432d99496c37cc94e0ee3a6a8d03dd494
Taylor McGough <taylorm@qwyit.com>
2016-05-28 13:56:35 -0400

    IO print array developed

---
+++ QwyitCipher/IO.c
@@ -0,0 +1,13 @@
+#include "IO.h"
+
+void PrintArray(const uint8_t * array, const uint32_t length)
+{
+        uint32_t i;
+        for(i = 0; i < length; i++)
+        {
+                printf("%02x",array[i]);
+
+        }
+        printf("\n");
+}
+
---
+++ QwyitCipher/IO.h
@@ -0,0 +1,12 @@
+#ifndef IO_H_
+#define IO_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+void PrintArray(const uint8_t * a, const uint32_t len);
+
+#endif
