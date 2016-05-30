7a7e4de3d8995583957d6a2438b8254ceb2d5045
Taylor McGough <taylorm@qwyit.com>
2016-05-30 17:17:43 -0400

    initial CompressTree Implemenation

---
+++ FileCompression/CompressTree.c
@@ -0,0 +1,9 @@
+#include "CompressTree.h"
+#include "../QwyitCipher/CipherQ.h"
+#include "../QwyitCipher/IO.h"
+
+uint8_t * CompressTree(const uint8_t * m, const uint32_t m_len)
+{
+
+
+}
---
+++ FileCompression/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "../QwyitCipher/IO.h"
+
+uint32_t main(void)
+{
+
+	printf("Compression:\n");
+	uint8_t array[10] = {0};
+	PrintArray(array, 10);
+}
