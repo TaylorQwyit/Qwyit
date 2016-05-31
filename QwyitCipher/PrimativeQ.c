280bbc150631411421c2f6e38b922ba07fa23794
Taylor McGough <taylorm@qwyit.com>
2016-05-31 12:09:15 -0400

    All Parties Operating Agreement

--- QwyitCipher/PrimativeQ.c
+++ QwyitCipher/PrimativeQ.c
@@ -15,7 +15,7 @@ void ModEncrypt(const uint8_t * key1, const uint8_t * key2, uint8_t * result)
 	uint32_t i = 0;
 	for(i; i < LENGTH; i++)
 	{
-                uint64_t carry = ( *(uint8_t *)(K1+i) & key2[i] & MODMASK); 
+                uint64_t carry = ( *(uint8_t *)(K1+i) & *(uint8_t *)(K2+i) & MODMASK); 
                 result[i] = key1[i] ^ key2[i];
                 carry = carry<<1;
                 while(carry != 0)
