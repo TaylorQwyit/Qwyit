a317bb49759c84ae89c731907f352a837efb1583
Taylor McGough <taylorm@qwyit.com>
2016-06-07 13:49:14 -0400

    Unlock and Lock implemented, not a communative cipher

--- QwyitCipher/main.c
+++ QwyitCipher/main.c
@@ -19,87 +19,24 @@ int main(void)
 
 	unsigned char m1[32] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
 
-
-/*
 	uint8_t c1[32] = {0};
 	uint8_t c2[32] = {0};
 	uint8_t r1[32] = {0};
 	uint8_t r2[32] = {0};
 
-	uint8_t mask1[8] = {0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77};
+	uint8_t mask1[8] = {0x7f, 0xaf, 0x44, 0x20, 0x12, 0xFf, 0x3f, 0x7f};
 
 	uint8_t mask2[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
         ModLock(m1, k1, c1, mask1);
 
-	ModLock(c1, k2, c2, mask2);
+	ModLock(c1, k2, c2, mask1);
 
         ModUnlock(c2, k1, r1, mask1);
 
-	ModUnlock(r1, k2, r2, mask2);
-
-*/	
+	ModUnlock(r1, k2, r2, mask1);
 	
-/*
-	uint8_t c1[32] = {0};
-		
-        ModEncrypt(k1, m1, c1);
 	
-	printf("m1: ");
-	PrintCharArray(m1,LENGTH);
-	printf("k1: ");
-	PrintCharArray(k1,LENGTH);
-	printf("c1: ");
-	PrintCharArray(c1,LENGTH);
-*/	
-
-/*
-	uint8_t c1[32] = {0x03, 0x69, 0xcf, 0x25};
-	uint8_t c2[32] = {0};
-	
-        ModEncrypt(k2, c1, c2);
-	
-	printf("c1: ");
-	PrintCharArray(c1,LENGTH);
-	printf("k2: ");
-	PrintCharArray(k2,LENGTH);
-	printf("c2: ");
-	PrintCharArray(c2,LENGTH);
-*/
-
-/*
-	//uint8_t c2[32] = {0x05, 0x18, 0xa3, 0xb6};
-	uint8_t c2[32] = {0x01, 0xd6, 0x2b, 0xb4};
-	
-	uint8_t r1[32] = {0};
-	
-        ModDecrypt(c2, k1, r1);
-	printf("c2: ");
-	PrintCharArray(c2,LENGTH);
-	printf("k1: ");
-	PrintCharArray(k1,LENGTH);
-	printf("r1: ");
-	PrintCharArray(r1,LENGTH);
-*/
-
-
-///*
-	//uint8_t r1[32] = {0x03,0xd2, 0x29,0xf8};
-
-	uint8_t r1[32] = {0x0f, 0x90, 0xa1, 0xf6};
-	uint8_t r2[32] = {0};
-        ModDecrypt(r1, k2, r2);
-	
-	printf("r1: ");
-	PrintCharArray(r1,LENGTH);
-	printf("k2: ");
-	PrintCharArray(k2,LENGTH);
-	printf("r2: ");
-	PrintCharArray(r2,LENGTH);
-	printf("m1: ");
-	PrintCharArray(m1,LENGTH);
-//*/
-
 		
 /*
 	printf("QwyitCipher: main\n");
